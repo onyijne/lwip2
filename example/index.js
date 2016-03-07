f06b233ec28d1445024bc136bd667ac704c79444
Ionică Bizău <bizauionica@gmail.com>
2016-03-07 10:15:28 +0200

    :rocket:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,11 @@
 
 const lwip2 = require("../lib");
 
-console.log(lwip2());
+lwip2.open("foo", err => {
+    console.log(err);
+    // In case the lwip package failed to be compiled
+    // =>
+    // { [Error: Failed to load lwip.]
+    //   reason: { [Error: Cannot find module 'lwip'] code: 'MODULE_NOT_FOUND' },
+    //   code: 'CANNOT_LOAD_LWIP' }
+});
